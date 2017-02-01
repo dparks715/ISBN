9ac132e3952aa308e6ce4b6cb96a294d860259e1
Daniel Parks <dparks715@yahoo.com>
2017-02-01 15:34:53 -0500

    making sure passing an array doesnt work

--- ISBN.rb
+++ ISBN.rb
@@ -1,7 +1,7 @@
 def check_isbn(isbn_num)
 	if /X/.match(isbn_num[-1]) == nil && /\d/.match(isbn_num[-1]) == nil
 		false
-	else
+	else /\A/.match(isbn_num)
 		true
 	end
 
--- test_ISBN.rb
+++ test_ISBN.rb
@@ -2,14 +2,15 @@ require 'minitest/autorun'
 require_relative 'ISBN.rb'
 
 class TestISBNFunction < Minitest::Test
-#	def test_ISBN_is_string
-#		isbn = '12345'
-#		assert_equal(String, check_isbn(isbn))	
-#	end
+	def test_ISBN_is_string
+		array = [12345]
+		results = check_isbn(array)
+		assert_equal(true, results)	
+	end
 
 	def test_last_char
-		isbn = check_isbn('12345')
-		assert_equal(true, isbn)
+		results = check_isbn('12345')
+		assert_equal(true, results)
 	end
 
 end
