c8bb1e675b1b4dad73c2f1764a5f597f5a8dfdb6
Daniel Parks <dparks715@yahoo.com>
2017-02-01 15:53:02 -0500

    refactor for legibility

--- ISBN.rb
+++ ISBN.rb
@@ -1,11 +1,12 @@
 def check_isbn(isbn_num)
+	valid = false
 	#checks the last index position of isbn_num for 0-9 or X
 	#if both conditions are not met, ISBN is not valid, returns false
-	if /X/.match(isbn_num[-1]) == nil && /\d/.match(isbn_num[-1]) == nil
-		false
-	#checks that isbn_num is the beginning of a string, then returns true
-	else /\A/.match(isbn_num)
-		true
+	if /\A/.match(isbn_num) &&
+		!!/X/.match(isbn_num[-1]) == true ||
+		!!/\d/.match(isbn_num[-1]) == true
+		
+		valid = true
 	end
-
+	valid
 end
