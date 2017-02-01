16c352e22b8f68cb0dc01db79aedf371e0f058bb
Daniel Parks <dparks715@yahoo.com>
2017-02-01 15:36:14 -0500

    added test for check digit X

--- test_ISBN.rb
+++ test_ISBN.rb
@@ -3,13 +3,17 @@ require_relative 'ISBN.rb'
 
 class TestISBNFunction < Minitest::Test
 	def test_ISBN_is_string
-		array = [12345]
-		results = check_isbn(array)
+		results = check_isbn('12345')
 		assert_equal(true, results)	
 	end
 
 	def test_last_char
 		results = check_isbn('12345')
+		assert_equal(true, results)
+	end
+
+	def test_last_char_X
+		results = check_isbn('12345X')
 		assert_equal(true, results)
 	end
 
