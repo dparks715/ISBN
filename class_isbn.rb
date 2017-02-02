cd0752c7a77b197655c0a8a0d0e027084f5085a6
Daniel Parks <dparks715@yahoo.com>
2017-02-02 10:15:22 -0500

    new function to remove spaces

--- class_isbn.rb
+++ class_isbn.rb
@@ -1,7 +1,12 @@
-def check_isbn(isbn_num)
+def isbn_length?(isbn_num)
+#passing in a string, want boolean returned
 	if isbn_num.length == 10
 		true
 	else
 		false
 	end
+end
+
+def remove_spaces(isbn_num)
+	isbn_num.delete(' ')
 end
--- test_class_isbn.rb
+++ test_class_isbn.rb
@@ -1,16 +1,30 @@
 require 'minitest/autorun'
 require_relative 'class_isbn.rb'
 
-class TestISBNFunction < Minitest::Test
+class TestISBNLength < Minitest::Test
 
 	def test_ten_length_true
-		results = check_isbn('1234567890')
+		results = isbn_length?('1234567890')
 		assert_equal(true, results)	
 	end
 
 	def test_nine_length_false
-		results = check_isbn('123456789')
+		results = isbn_length?('123456789')
 		assert_equal(false, results)	
+	end
+
+	def test_ten_length_alphabet_true
+		results = isbn_length?('abcdefghij')
+		assert_equal(true, results)	
+	end
+
+end
+
+class TestRemoveSpaces < Minitest::Test
+
+	def test_remove_spaces
+		results = remove_spaces('12  3 4 5')
+		assert_equal('12345', results)
 	end
 
 end
