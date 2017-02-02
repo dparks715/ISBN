81b4c2b9ffcd737345577c0e7732e7d48599404a
Daniel Parks <dparks715@yahoo.com>
2017-02-02 08:48:35 -0500

    added test for false

--- test_class_isbn.rb
+++ test_class_isbn.rb
@@ -2,8 +2,15 @@ require 'minitest/autorun'
 require_relative 'class_isbn.rb'
 
 class TestISBNFunction < Minitest::Test
-	def test_ISBN_is_string
+
+	def test_ten_length_true
 		results = check_isbn('1234567890')
 		assert_equal(true, results)	
 	end
+
+	def test_nine_length_false
+		results = check_isbn('123456789')
+		assert_equal(false, results)	
+	end
+
 end
